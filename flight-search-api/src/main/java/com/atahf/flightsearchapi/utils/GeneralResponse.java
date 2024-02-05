94c21d9eea775a8c7eca2e51f4b1cae2d1d73944
atahf <ataollah@sabanciuniv.edu>
2024-02-05 11:51:31 +0300

    response documentations

--- flight-search-api/src/main/java/com/atahf/flightsearchapi/utils/GeneralResponse.java
+++ flight-search-api/src/main/java/com/atahf/flightsearchapi/utils/GeneralResponse.java
@@ -1,13 +1,19 @@
 package com.atahf.flightsearchapi.utils;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "General Response Model")
 public class GeneralResponse<T> {
+    @ApiModelProperty(value = "status of response object, for cases of 2xx, 4xx response codes")
     private String status;
+    @ApiModelProperty(value = "number of results, if result type is an object or generic type it will be 1, otherwise it will be size of list")
     private int result_count;
+    @ApiModelProperty(value = "results of the requested operation")
     private T result;
 }
